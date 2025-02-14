8156b7f71237e44edf397b811ea6ebb0c79d3b3f
amirpoudel <amirpoudel2058@gmail.com>
2025-02-14 22:56:23 +0545

    question to vector

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from gensim.models import Word2Vec # type: ignore
 import json
+import numpy as np # type: ignore
 
 # Load labeled data
 with open('./dataset/sample_label_data.json', 'r', encoding='utf-8') as f:
@@ -9,18 +10,56 @@ with open('./dataset/sample_label_data.json', 'r', encoding='utf-8') as f:
 with open('./dataset/sample_unlable_data.json', 'r', encoding='utf-8') as f:
     unlabeled_data = json.load(f)
 
-# Print data to check
-# print(labeled_data)
-# print(unlabeled_data)
+# need to work on this later
+# def preprocess_question(question):
+#     # Normalize (lowercase, remove non-alphabetic characters)
+#     question = re.sub(r'[^a-zA-Z0-9\s]+', '', question.lower())
+    
+#     # Tokenize
+#     tokens = word_tokenize(question)
+    
+#     # Apply stemming
+#     tokens_stemmed = [stemmer.stem(word) for word in tokens]
+    
+#     return tokens_stemmed
+
+# # Apply preprocessing to your labeled dataset
+# labeled_data_processed = []
+# for entry in labeled_data:
+#     processed_question = preprocess_question(entry["question"])
+#     labeled_data_processed.append(processed_question)
+
+# print(labeled_data_processed)
 
 
 model = "./processed.word2vec"
 word2vec_model = Word2Vec.load(model)
 
-def question_to_vector(word):
-    return word2vec_model.wv[word]
 
-print(question_to_vector("नेपाल"))
+
+# Function to get sentence vector (by averaging word vectors)
+def get_question_vector(question, word2vec_model):
+    # Preprocess the question (tokenize and stem)
+    processed_question = question
+    
+    # Get word vectors for each token in the question
+    word_vectors = []
+    for word in processed_question:
+        if word in word2vec_model.wv:
+            word_vectors.append(word2vec_model.wv[word])
+    
+    # If there are no word vectors (no words in Word2Vec model), return zero vector
+    if word_vectors:
+        return np.mean(word_vectors, axis=0)
+    else:
+        return np.zeros(word2vec_model.vector_size)
+
+# Example usage
+question = "नेपालको पहिलो राष्ट्रपति को हुन्?"
+question_vector = get_question_vector(question, word2vec_model)
+print(question_vector)
+
+
 
 
 
