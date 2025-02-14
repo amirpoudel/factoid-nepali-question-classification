7ba4678e11df6be544d8b5ae814c5d2db276a4fc
amirpoudel <amirpoudel2058@gmail.com>
2025-02-14 22:41:23 +0545

    setup

---
+++ main.py
@@ -0,0 +1,28 @@
+from gensim.models import Word2Vec # type: ignore
+import json
+
+# Load labeled data
+with open('./dataset/sample_label_data.json', 'r', encoding='utf-8') as f:
+    labeled_data = json.load(f)
+
+# Load unlabeled data
+with open('./dataset/sample_unlable_data.json', 'r', encoding='utf-8') as f:
+    unlabeled_data = json.load(f)
+
+# Print data to check
+# print(labeled_data)
+# print(unlabeled_data)
+
+
+model = "processed.word2vec"
+word2vec_model = Word2Vec.load("processed.word2vec")
+
+def question_to_vector(word):
+    return word2vec_model.wv[word]
+
+print(question_to_vector("नेपाल"))
+
+
+
+
+
